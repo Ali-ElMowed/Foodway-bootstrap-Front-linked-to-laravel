e821d0525f2ecc76c8153ca7762b239a86043587
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-08 11:24:58 +0300

    Added the fetch function to ger all restos

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,48 @@
-/*!
-* Start Bootstrap - Small Business v5.0.5 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+let resto_cards = document.getElementById('resto-cards');
+
+
+
+let data = new FormData();
+
+fetch('http://127.0.0.1:8000/api/restaurants',
+    {
+        method: 'GET'
+    }
+
+)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let card = '';
+        for (let i = 0; i < data.restos.length; i++) {
+            let restaurant = data.restos[i];
+            console.log(restaurant);
+            let restaurant_name = restaurant.resto_name;
+            let restaurant_id = restaurant.id;
+            let restaurant_description = restaurant.description;
+            let restaurant_phone = restaurant.phone_number;
+            let restaurant_city = restaurant.city_id;
+            let restaurant_category = restaurant.cat_id;
+
+            card=`
+            <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">${restaurant_name}</h2>
+                            <p class="card-text">${restaurant_description}</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+            `;
+            
+            resto_cards.innerHTML += card;
+
+    
+        }
+    
+    })
+       
+            
+
